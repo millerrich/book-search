6047efb328be4a5d2fee440533573e4248ae94b1
Miller Rich <william.miller.rich@gmail.com>
2020-10-23 16:35:18 -0500

    update server.js to require routes directory, app.use(routes)

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
