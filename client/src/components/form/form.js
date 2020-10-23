f0ea6cfd7304c96113626ba8d62ba4e2909298e0
Miller Rich <william.miller.rich@gmail.com>
2020-10-22 22:14:07 -0500

    create function searchBooks and handleInputChange in Search.js, add search bar to Search.js, add axios get method for googlebooks api in API.js

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function Form() {
+    function handleInputChange(event) {
+        console.log(event.target.value);
+         
+    }
     return(
         <form className="bg-light">
             <div className="form-group">
                 <label for="search">Search:</label>
-                <input type="text" className="form-control" id="search" aria-describedby="bookSearch" placeholder="Search for a book" />
+                <input type="text" className="form-control" id="search" aria-describedby="bookSearch" placeholder="Search for a book" onChange={handleInputChange} />
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </form>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar/nav';
-import Form from '../components/form/form';
+import API from '../utils/API';
+
 
 function Search() {
+    
+    const [results, setResults] = useState([]);
+
+
+    
+    function searchBooks(search) {
+        API.googleBook(search)
+        .then(res => setResults(res.data.items));
+        console.log(results);
+    }
+
+    function handleInputChange(event) {
+        console.log(event.target.value);
+        searchBooks(event.target.value);
+    }
     return (
         <div className="container">
         <Navbar />
-        <Form />
+        <form className="bg-light">
+            <div className="form-group">
+                <label htmlFor="search">Search:</label>
+                <input type="text" className="form-control" id="search" aria-describedby="bookSearch" placeholder="Search for a book" onChange={handleInputChange} />
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+        </form>
         <h1>SEARCH PAGE</h1>
         </div>
     )
