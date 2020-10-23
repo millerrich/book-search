fb30332b60c6ba8129d048366aa2b3c0c5ff78ba
Miller Rich <william.miller.rich@gmail.com>
2020-10-22 19:17:18 -0500

    add navbar component, basic search page and saved page

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/navbar/nav';
+
+function Saved() {
+    return (
+        <div className="container">
+        <Navbar />
+        <h1>SAVED PAGE</h1>
+        </div>
+    )
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/navbar/nav';
+
+function Search() {
+    return (
+        <div className="container">
+        <Navbar />
+        <h1>SEARCH PAGE</h1>
+        </div>
+    )
+}
+
+export default Search;
