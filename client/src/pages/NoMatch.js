0668c1e2bbce024e54ccea55b3ee5ec3b04c3f68
Miller Rich <william.miller.rich@gmail.com>
2020-10-22 19:24:08 -0500

    app.js routes setup, add NoMatch.js page

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from '../components/navbar/nav';
+
+
+function NoMatch() {
+    return(
+        <div>
+            <Navbar />
+            <h1>404 Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NoMatch;
