b1bbda387ada9a9ab57c7bf40b8269476e84a660
Miller Rich <william.miller.rich@gmail.com>
2020-10-22 23:48:40 -0500

    make changes in Search.js and card.js to attempt to prevent error when thumbnail is undefined

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card(props) {
     return (
         <div className="card" style={{width: '18rem'}}>
-            <img className="card-img-top" src={props.imageLinks.thumbnail} alt={props.title} />
+            <img className="card-img-top" src={props.imageLinks.thumbnail === undefined ? "" : props.imageLinks.thumbnail} alt={props.title} />
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}</p>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,27 +6,30 @@ import Card from '../components/card/card';
 
 function Search() {
     
+    const [book, setBook] = useState('');
     const [results, setResults] = useState([]);
 
 
     
-    function searchBooks(search) {
-        API.googleBook(search)
+    function searchBooks(event) {
+        event.preventDefault()
+        API.googleBook(book)
         .then(res => {
-            console.log(res.data.items)
+            // console.log(res.data.items)
             const searchResults = res.data.items;
             const mapped = [];
             // console.log("search result", searchResults);
             searchResults.map(o => mapped.push(o.volumeInfo));
             
             setResults(mapped)
-            // console.log("results", results);
-        });
+            console.log("results", results);
+        })
+        .catch(err => console.log(err));
     }
 
     function handleInputChange(event) {
         console.log(event.target.value);
-        searchBooks(event.target.value);
+        setBook(event.target.value);
     }
     return (
         <div className="container">
@@ -35,7 +38,7 @@ function Search() {
             <div className="form-group">
                 <label htmlFor="search">Search:</label>
                 <input type="text" className="form-control" id="search" aria-describedby="bookSearch" placeholder="Search for a book" onChange={handleInputChange} />
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary" onClick={searchBooks}>Submit</button>
             </div>
         </form>
         <div className="container d-flex flex-row flex-wrap">
